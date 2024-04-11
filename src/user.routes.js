22df17dbe9bd2f1540facc98061df8ef39ebd80b
Toni Inot <tonigwapo@gmail.com>
2024-04-11 22:03:39 +0800

    webhook test

--- src/user.routes.js
+++ src/user.routes.js
@@ -1,132 +1,75 @@
 import express from "express";
-import userController from "./controllers/user.controller"
+import userController from "./controllers/user.controller";
 const router = express.Router();
-const FileUpload = require('express-fileupload')
+const FileUpload = require("express-fileupload");
 
-router.use(FileUpload())
+router.use(FileUpload());
 
+router.post("/webhooktest", function (req, res) {
+  const data = req;
+
+  return res.json({
+    message: "hola",
+    data,
+  });
+});
+
+router.get("/webhooktest", function (req, res) {
+  const data = { eeee: "eeee" };
+
+  return res.json({
+    message: "hola",
+    data,
+  });
+});
 
 //================================Admin APIs====================================//
-router.get(
-    '/allreports',
-    userController.getAllReports
-)
+router.get("/allreports", userController.getAllReports);
 
-router.get(
-    '/reports/:id',
-    userController.getReports
-)
+router.get("/reports/:id", userController.getReports);
 
-router.delete(
-    '/reports/:id',
-    userController.banParking
-)
+router.delete("/reports/:id", userController.banParking);
 
-router.post(
-    '/report',
-    userController.addReport
-)
+router.post("/report", userController.addReport);
 //================================parking APIs========================//
-router.get(
-    '/all',
-    userController.getAllParking
-)
+router.get("/all", userController.getAllParking);
 
-router.get(
-    '/:id',
-    userController.getParking
-)
+router.get("/:id", userController.getParking);
 
-router.post(
-    '/create',
-    userController.addParking
-)
+router.post("/create", userController.addParking);
 
-router.delete(
-    '/:id',
-    userController.deleteParking
-)
+router.delete("/:id", userController.deleteParking);
 
-router.put(
-    '/:id',
-    userController.editParking
-)
+router.put("/:id", userController.editParking);
 
-router.post(
-    '/img/:username',
-    userController.uploadParkingImage
-)
+router.post("/img/:username", userController.uploadParkingImage);
 
-router.get(
-    '/img/:username/:parkingName',
-    userController.getParkingImage
-)
+router.get("/img/:username/:parkingName", userController.getParkingImage);
 
-router.get(
-    '/myparking/:username',
-    userController.getMyParking
-)
+router.get("/myparking/:username", userController.getMyParking);
 //============================user APIs=================================//
-router.post(
-    '/signup',
-    userController.signUp
-)
+router.post("/signup", userController.signUp);
 
-router.post(
-    '/signin',
-    userController.signIn
-)
+router.post("/signin", userController.signIn);
 
-router.post(
-    '/signout',
-    userController.signOut
-)
+router.post("/signout", userController.signOut);
 
-router.get(
-    `/getuser/:token`,
-    userController.getUser
-)
+router.get(`/getuser/:token`, userController.getUser);
 
-router.get(
-    `/getuserinfo/:id`,
-    userController.getUserInfo
-)
+router.get(`/getuserinfo/:id`, userController.getUserInfo);
 
-router.get(
-    `/getsession`,
-    userController.getSessions
-)
+router.get(`/getsession`, userController.getSessions);
 
-router.put(
-    '/updateuser/:id',
-    userController.updateUser
-)
+router.put("/updateuser/:id", userController.updateUser);
 
-router.post(
-    '/adduserinfo',
-    userController.addUserInfo
-)
+router.post("/adduserinfo", userController.addUserInfo);
 
-router.post(
-    '/resetpassword',
-    userController.resetPassword
-)
+router.post("/resetpassword", userController.resetPassword);
 
-router.post(
-    '/updatepassword',
-    userController.updatePassword
-)
+router.post("/updatepassword", userController.updatePassword);
 
-router.post(
-    '/addparkinghistory',
-    userController.addParkingHistory
-)
+router.post("/addparkinghistory", userController.addParkingHistory);
 
-router.get(
-    '/getparkinghistory/:user',
-    userController.getParkingHistory
-)
+router.get("/getparkinghistory/:user", userController.getParkingHistory);
 
-
-
-export default router
+export default router;
